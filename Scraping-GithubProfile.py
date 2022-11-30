d82dfbe3d2b5bb9228878b48844f669e6bab2c62
Ahmet Can <ahmetcanckr37@gmail.com>
2022-11-30 06:35:09 +0300

    a

--- Scraping-GithubProfile.py
+++ Scraping-GithubProfile.py
@@ -9,17 +9,3 @@ soup = BeautifulSoup(request.content, 'html.parser')
 #Get the avatar image of the user
 profileImage = soup.find('img', {'alt' : 'Avatar'})['src']
 print(profileImage)
-
-#Get the bio of the user
-profileBio=soup.find("div",{"class":"p-note user-profile-bio mb-3 js-user-profile-bio f4"})
-print(githubUser + "'s : " + profileBio.text)
-
-#Get the name of the user
-profileName=soup.find("span",{"class":"p-name vcard-fullname d-block overflow-hidden"})
-print("Name of the User: " + profileName.text)
-
-#Get the followers of the user
-profileFollowers=soup.find("span",{"class":"text-bold color-fg-default"})
-print("Followers: " + profileFollowers.text)
-
-
